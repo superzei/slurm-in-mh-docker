be24a8a9f275ecedbbf280a73090451e968e7128
hakanakyurek <b21426553@cs.hacettepe.edu.tr>
2019-01-03 23:21:51 +0300

    Configration file added, example scripts changed respectively.

--- manager/example_script/script.cu
+++ manager/example_script/script.cu
@@ -7,14 +7,17 @@ void saxpy(int n, float a, float *x, float *y)
   if (i < n) y[i] = a*x[i] + y[i];
 }
 
-int main(void)
+int main(int argc, char **argv)
 {
+
+  printf(argv[1]);
+  printf(argv[2]);
   int N = 1<<20;
   float *x, *y, *d_x, *d_y;
   x = (float*)malloc(N*sizeof(float));
   y = (float*)malloc(N*sizeof(float));
 
-  cudaMalloc(&d_x, N*sizeof(float)); 
+  cudaMalloc(&d_x, N*sizeof(float));
   cudaMalloc(&d_y, N*sizeof(float));
 
   for (int i = 0; i < N; i++) {
@@ -40,3 +43,4 @@ int main(void)
   free(x);
   free(y);
 }
+
